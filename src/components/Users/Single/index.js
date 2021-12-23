1b5baf51712a4731e266f214579bdb3b21fd0677
Martin Giura <martingiura.dev@gmail.com>
2021-12-23 13:43:32 -0600

    mejoras en perfil individual

--- src/components/Users/Single/index.js
+++ src/components/Users/Single/index.js
@@ -13,17 +13,6 @@ export default function SingleUser() {
   return (
     <>
       <div>
-        <div class="mt-4 flex md:mt-0">
-          <Link to={`/guitarras/${id}/editar`}>
-            <button
-              type="button"
-              class="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              Editar Usuario
-            </button>
-          </Link>
-        </div>
-
         <div className="bg-white">
           <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-x-8">
             <div className="lg:max-w-lg lg:self-end">
@@ -61,8 +50,17 @@ export default function SingleUser() {
                 </ol>
               </nav>
               <div className="mt-4">
-                <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                  {singleUser.nombre}
+                <h1 className="text-3xl font-extrabold tracking-tight text-blue-600 sm:text-4xl">
+                  Nombre
+                  <h1 className="mt-6 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                    {singleUser.nombre}
+                  </h1>
+                  <button
+                    type="button"
+                    class="mt-6 ml-0 mr-6 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-orange-400 hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Quiero conectar con {singleUser.nombre}
+                  </button>
                 </h1>
               </div>
               <section aria-labelledby="information-heading" className="mt-4">
@@ -70,8 +68,9 @@ export default function SingleUser() {
                   Nombre
                 </h2>
                 <div className="flex items-center">
-                  <p className="text-lg text-gray-900 sm:text-xl">
-                    Profile Summary: {singleUser.profileSummary}
+                  <p className="text-lg text-blue-600 sm:text-xl">
+                    Perfil Profesional:
+                    <p>{singleUser.profileSummary}</p>
                   </p>
                   <div className="ml-4 pl-4 border-l border-gray-300">
                     <h2 className="sr-only">Reviews</h2>
@@ -131,7 +130,9 @@ export default function SingleUser() {
                   </div>
                 </div>
                 <div className="mt-4 space-y-6">
-                  <p className="text-base text-gray-500">{singleUser.budget}</p>
+                  <p className="text-base text-gray-500">
+                    Presupuesto mensual: $MNX {singleUser.budget}
+                  </p>
                 </div>
                 <div className="mt-6 flex items-center">
                   <svg
@@ -147,9 +148,32 @@ export default function SingleUser() {
                       clipRule="evenodd"
                     />
                   </svg>
-                  <p className="ml-2 text-sm text-gray-500">
-                    In stock and ready to ship
-                  </p>
+                  <div className="text-3xl font-extrabold tracking-tight text-blue-600 sm:text-xl">
+                    ¿Con qué frecuencia limpias tu cuarto?
+                    <div className="mt-8 text-lg text-gray-900 sm:text-xl">
+                      {singleUser.frequencyCleaningRoom}
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-8 text-3xl font-extrabold tracking-tight text-blue-600 sm:text-xl">
+                  ¿Fumas?
+                  <div className="mt-8 text-lg text-gray-900 sm:text-xl">
+                    {singleUser.smokingPreferences}
+                  </div>
+                </div>
+
+                <div className="mt-8 text-3xl font-extrabold tracking-tight text-blue-600 sm:text-xl">
+                  ¿Que piensas acerca de las mascotas?
+                  <div className="mt-8 text-lg text-gray-900 sm:text-xl">
+                    {singleUser.petsPreferences}
+                  </div>
+                </div>
+
+                <div className="mt-8 text-3xl font-extrabold tracking-tight text-blue-600 sm:text-xl">
+                  ¿Que piensas acerca de las visitas a la casa o departamento?
+                  <div className="mt-8 text-lg text-gray-900 sm:text-xl">
+                    {singleUser.visitPreferences}
+                  </div>
                 </div>
               </section>
             </div>
@@ -157,17 +181,13 @@ export default function SingleUser() {
               <div className="aspect-w-1 aspect-h-1 rounded-lg overflow-hidden">
                 <img
                   src={singleUser.imageUrl}
-                  alt="Imagen de guitarra"
+                  alt={`Imagen de ${singleUser.nombre}`}
                   className="w-full h-full object-center object-cover"
                 />
               </div>
             </div>
             <div className="mt-10 lg:max-w-lg lg:col-start-1 lg:row-start-2 lg:self-start">
-              <section aria-labelledby="options-heading">
-                <h2 id="options-heading" className="sr-only">
-                  Product options
-                </h2>
-              </section>
+              <section aria-labelledby="options-heading"></section>
             </div>
           </div>
         </div>
